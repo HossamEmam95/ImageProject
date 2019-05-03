5d722260799529e28f7743f501b88096df93e127
hossa95 <hossamsalahemam@gmail.com>
2019-05-04 00:18:24 +0200

    update take image

--- ocr.py
+++ ocr.py
@@ -1,7 +1,3 @@
-import os
-import string
-import argparse
-import datefinder
 import pytesseract
 
 def run_ocr(image)->str:
@@ -11,10 +7,3 @@ def run_ocr(image)->str:
 	ocrOutput = pytesseract.image_to_string(image)
 	print(ocrOutput)
 	return ocrOutput
-
-if __name__ == '__main__':
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-i", "--image", required = True,
-	help = "path to the image file")
-	args = vars(ap.parse_args())
-	run_ocr(args['image'])
