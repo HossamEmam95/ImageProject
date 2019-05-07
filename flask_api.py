5cebb0ab2c7cd5c229b371a73c1284ba237f9804
hossa95 <hossamsalahemam@gmail.com>
2019-05-07 03:13:49 +0200

    seprate 2 apis

--- flask_api.py
+++ flask_api.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, Response
 import os
 from barcode_read import run
-import numpy as np
+from ocr import run_ocr
 import cv2
 
 # Initialize the Flask application
@@ -9,18 +9,36 @@ app = Flask(__name__)
 folder = './'
 app.config['UPLOAD_FOLDER'] = folder
 
-# route http posts to this method
-@app.route('/', methods=['POST'])
-def test():
-    image = request.files['image']
+# barcode api
+@app.route('/barcode', methods=['POST'])
+def barcode():
+    image = request.files['img']
 
     image.save(os.path.join(app.config['UPLOAD_FOLDER'], "down.jpg"))
 
     img = cv2.imread('down.jpg')
     decoded_obj = run(img)
-    print(decoded_obj[0].data)
-    return "hi"
+    if len(decoded_obj) > 0:
+        return decoded_obj[0].data
+    else:
+        return "Sorry can find the barcode can you retake a more clear image"
+
+
+# ocr api
+@app.route('/ocr', methods=['POST'])
+def ocr_api():
+    image = request.files['img']
+
+    image.save(os.path.join(app.config['UPLOAD_FOLDER'], "down.jpg"))
+
+    img = cv2.imread('down.jpg')
+    text = run_ocr(img)
+    pos = text.find('EXP')
+    date = text[pos-2: pos] + text[pos+1: pos+3]
+    print(text)
+    return date
 
 # start flask app
-app.run(debug=True, host= '0.0.0.0')
+app.run(debug=True)
+
 # to run this on local: FLASK_APP=flask_api.py flask run --host=0.0.0.0
